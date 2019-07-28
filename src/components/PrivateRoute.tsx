48d5d8c4ed8bd4bf5b90ecf9cd29f349cc8864bb
Musaab Elfaqih <musabelfaki@gmail.com>
2019-07-28 10:25:09 +0200

    Add profile view

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,27 +1,38 @@
 import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
 import { useAuth0 } from "../contexts/auth";
+import LoadingView from "./LoadingView";
 
 interface PrivateRouteProps {
-  component: React.JSXElementConstructor<any>
-  path: string
+  component: React.JSXElementConstructor<any>;
+  exact: boolean;
+  path: string;
 }
 
-const PrivateRoute = ({ component: Component, path, ...rest }: PrivateRouteProps) => {
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
+const PrivateRoute = ({
+  component: Component,
+  path,
+  ...rest
+}: PrivateRouteProps) => {
+  const { isAuthenticated, loginWithRedirect, loading } = useAuth0();
 
   useEffect(() => {
     const fn = async () => {
-      if (!isAuthenticated) {
+      if (!loading && !isAuthenticated) {
         await loginWithRedirect({
           appState: { targetUrl: path }
         });
       }
     };
     fn();
-  }, [isAuthenticated, loginWithRedirect, path]);
+  }, [loading, isAuthenticated, loginWithRedirect, path]);
 
-  const render = (props: any) => isAuthenticated === true ? <Component {...props} /> : null;
+  const render = (props: any) =>
+    loading === true ? (
+      <LoadingView />
+    ) : isAuthenticated === true ? (
+      <Component {...props} />
+    ) : null;
 
   return <Route path={path} render={render} {...rest} />;
 };

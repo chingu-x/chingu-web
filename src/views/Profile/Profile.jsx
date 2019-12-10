9cb957756bc7c66cfd130755a5642d8655153de6
Musaab Elfaqih <musabelfaki@gmail.com>
2019-12-10 10:12:36 +0100

    Add articles

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -12,12 +12,17 @@ import {
   banner,
   bannerContent,
   userInfo,
-  dates,
   top,
   topText,
   heroImg,
-  steps,
-  stepsTitle
+  gettingStarted,
+  resources,
+  articles,
+  articleCard,
+  articleNumber,
+  articleSubtitle,
+  articleTitle,
+  articleBody
 } from './Profile.module.scss';
 import GoatGlassesWebp from './GoatGlasses.webp';
 import GoatGlassesPNG from './GoatGlasses.png';
@@ -31,6 +36,36 @@ const GET_EXISTING_APPLICATION = gql`
     }
   }
 `;
+
+function ArticleCard({ subtitle, title, body, number, color, link }) {
+  const cardContents = (
+    <>
+      {number && <span className={articleNumber}>{number}</span>}
+      <Paragraph size="small" className={articleSubtitle}>
+        {subtitle}
+      </Paragraph>
+      <Title level={3} className={articleTitle}>
+        {title}
+      </Title>
+      <Paragraph className={articleBody}>{body}</Paragraph>
+    </>
+  );
+  return link ? (
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={articleCard}
+      style={{ borderTopColor: color || '#13e58c' }}
+    >
+      {cardContents}
+    </a>
+  ) : (
+    <div className={articleCard} style={{ borderTopColor: color || '#13e58c' }}>
+      {cardContents}
+    </div>
+  );
+}
 
 export default function Profile() {
   const { data: applicationData = {}, loading: loadingApplication } = useQuery(
@@ -85,7 +120,7 @@ export default function Profile() {
                 {user.email}
               </Paragraph>
               <Paragraph size="small">
-                {isSolo ? 'Solo Project Path' : 'Voyage Member'}
+                {isSolo ? 'Solo Project Path' : 'Voyage Project Path'}
               </Paragraph>
               {isSolo && (
                 <Paragraph size="small">
@@ -146,161 +181,87 @@ export default function Profile() {
             )}
           </div>
         </div>
-        <div className={steps}>
-          <Title className={stepsTitle}>Next Steps</Title>
-          <br />
-          <ol>
-            <li>
-              <Title level={3}>
-                Read the{' '}
-                <a
-                  href="https://medium.com/chingu/chingu-voyage-teaser-extra-info-1c9abc0472c7"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Chingu Voyage Preview & extra info
-                </a>{' '}
-                article
-                <Paragraph size="small">(5 minutes)</Paragraph>
-              </Title>
-
-              <Paragraph>
-                So you don’t freak out before we start. It includes gifs,
-                overviews, past member articles/ project & experience
-                walkthroughs.
-              </Paragraph>
-            </li>
-            <li>
-              <Title level={3}>
-                Please check your email & whitelist chance@chingu.io
-              </Title>
-              <Paragraph>
-                A big part of the Voyage experience is being accoutable to your
-                goals, your project, and your team-mates. During the Voyage we
-                have a mandatory Weekly-Checkin (~3 minutes), which we send
-                every week via email. We use this both to help teams stay
-                accountable and as a tool to support teams that need assistance.
-                <br />
-                <br />
-                <a
-                  href="https://www.lifewire.com/how-to-whitelist-email-addresses-4588603"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Here is an article
-                </a>{' '}
-                that shows you how to do this.
-              </Paragraph>
-            </li>
-            <li>
-              <Title level={3}>Prepare for the Chingu Pre-work Session</Title>
-              <Paragraph>
-                Before each Voyage we run a short Pre-work session that lasts
-                approximately two weeks. This is so that you can get comfortable
-                with the communication tool we use (Discord), meet the
-                community, and submit your Pre-work project.
-                <br />
-                <br />
-                To maintain quality in teams, we require each member to submit a
-                deployed project before they are added to a team in the Voyage
-                session.
-                <br />
-                <br />
-                We encourage everyone to build one of the Chingu Prework
-                projects, but if you have a previous project you’d like to
-                submit instead, you can do that as well. You can read more about
-                it in the Chingu Handbook below.
-              </Paragraph>
-            </li>
-            <li>
-              <Title level={3}>
-                Go through{' '}
-                <a
-                  href="https://docs.chingu.io/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  the Chingu Handbook
-                </a>
-              </Title>
-              <Paragraph>
-                There’s a lot of useful material here. Below you can find the
-                key sections to read before your session starts so you can be
-                ready.
-                <br />
-                <br />
-                A){' '}
-                <a
-                  href="https://techres.docs.chingu.io/git/workflow"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn and/or review how to use Git/Github{' '}
-                </a>{' '}
-                (trust me, this is a big one)
-                <br />
-                <br />
-                B){' '}
-                <a
-                  href="https://voyage.docs.chingu.io/prep/getprepared/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Here are the basic To-dos to get started & prepare
-                </a>
-                <br />
-                <br />
-                C){' '}
-                <a
-                  href="https://voyage.docs.chingu.io/prework/submitprework/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Submit your Prework project
-                </a>
-                <br />
-                <br />D{' '}
-                <a
-                  href="https://chingu.docs.chingu.io/intro/schedule#cohort-13-schedule"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Here is the schedule for your Voyage
-                </a>
-              </Paragraph>
-            </li>
-            <li>
-              <Title level={3}>
-                Mark the Prework & Voyage launch dates in your calendar
-              </Title>
-              <Paragraph>
-                The dates are listed at the top of this screen. We’ll send you
-                an email with your Pre-work invite link at 7pm Rome time at the
-                above listed date.
-                <br />
-                <br />
-                (1pm in New York, 10am in San Fransisco, 10:30pm in Mumbai, 2am
-                in Tokyo, 5pm in Marrakech, 2pm in Rio de Janeiro)
-                <br />
-                <br />
-                We’ll launch your Voyage team in the same chat application
-                (Discord) as the Pre-work.
-              </Paragraph>
-            </li>
-          </ol>
-          <br />
-          <Paragraph style={{ textAlign: 'center' }} size="large" bold>
-            If you have any questions or feedback, feel free to reach out to us
-            at{' '}
-            <a
-              href="mailto:support@chingu.io"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              support@chingu.io
-            </a>
-          </Paragraph>
-          <br />
+        <div className={gettingStarted}>
+          <Title level={2}>Getting Started</Title>
+          <div className={articles}>
+            {isSolo ? (
+              <>
+                <ArticleCard
+                  number={1}
+                  subtitle="Read (3 minutes)"
+                  title="How can Chingu help me and how does it work?"
+                  body="This will tell you everything you need to know about the Solo project path."
+                  color="#000"
+                  link="https://solofree.docs.chingu.io/"
+                />
+                <ArticleCard
+                  number={2}
+                  subtitle="Read (4 minutes)"
+                  title="What will I build first and when do I start?"
+                  body="Build this and then we’ll give you something else to build."
+                  color="#000"
+                />
+                <ArticleCard
+                  number={3}
+                  subtitle="Connect with like-mind coders"
+                  title="Join the Chingu Community Discord"
+                  body="Post your intro, read about other coders from around the world, and ask questions."
+                  color="#000"
+                  link="https://discord.gg/XfhUYqe"
+                />
+              </>
+            ) : (
+              <>
+                <ArticleCard
+                  number={1}
+                  subtitle="Read (3 minutes)"
+                  title="Chingu Voyage Teaser & Extra info"
+                  body="This will give you an overview of the Chingu Voyage program to get you started."
+                  color="#000"
+                  link="https://medium.com/chingu/chingu-voyage-teaser-extra-info-1c9abc0472c7"
+                />
+                <ArticleCard
+                  number={2}
+                  subtitle="Requirements before the team project"
+                  title="Pre-work Requirements: Submit a Solo Project"
+                  body="To maintain quality, we require members to submit a completed project before being placed in a team."
+                  color="#000"
+                  link="https://voyage.docs.chingu.io/prework/howwork"
+                />
+                <ArticleCard
+                  number={3}
+                  subtitle="Connect with like-mind coders"
+                  title="Join the Chingu Community Discord"
+                  body="Post your intro, read about other coders from around the world, and ask questions."
+                  color="#000"
+                  link="https://discord.gg/XfhUYqe"
+                />
+              </>
+            )}
+          </div>
+        </div>
+        <div className={resources}>
+          <Title level={2}>Resources</Title>
+          <div className={articles}>
+            <ArticleCard
+              subtitle="Read"
+              title="The Chingu Handbook"
+              body="This is handbook breaks down everything in the Chingu program - from solo projects to team projects."
+              link="https://docs.chingu.io"
+            />
+            <ArticleCard
+              subtitle="Connect with like-mind coders"
+              title="Need help with a coding question? Ask here."
+              body="Note: you need to join the community Discord for this link to work (join @ #3 above)."
+              link="https://discordapp.com/channels/330284646283608064/553100045466206208"
+            />
+            <ArticleCard
+              subtitle="Read"
+              title="Learn about Git workflow & Github"
+              body="Everything you need to know about using Git & Github in Chingu."
+              link="https://techres.docs.chingu.io/git/workflow"
+            />
+          </div>
         </div>
       </Wrapper>
     </>

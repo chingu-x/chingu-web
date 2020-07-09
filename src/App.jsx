f4eb6ac72eba30c745a1a44c0bb2be3d215151b9
Musaab Elfaqih <musabelfaki@gmail.com>
2020-07-09 06:26:04 +0200

    Clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import Shell from './Shell';
-import Route from './components/Route';
-import Landing from './views/Landing';
-import Privacy from './views/Privacy';
-import HowItWorks from './views/HowItWorks';
-import Apply from './views/Apply';
-import SignIn from './views/SignIn';
-import Payment from './views/Payment';
-import Success from './views/Success';
-import Profile from './views/Profile';
-import SignOut from './views/SignOut';
-import NotFound from './views/NotFound';
 
 const App = () => {
   return <div>Undergoing maintenance</div>;
